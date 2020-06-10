5f37ed9b789435b3de362d694d96c0c908d0392e
Nicolas Gonzalez <ngonzalez@operametrix.fr>
2020-06-10 22:15:56 +0200

    Notify module for external notifications push

---
+++ notify/notify.go
@@ -0,0 +1,20 @@
+package notify
+
+import (
+	"github.com/spf13/viper"
+)
+
+type NotifyPlugins struct {
+	Telegram TelegramNotify
+}
+
+type NotifyConfig struct {
+	Notify NotifyPlugins
+}
+
+func Notify(message string) {
+	var config NotifyConfig
+	viper.Unmarshal(&config)
+
+	config.Notify.Telegram.Notify(message)
+}
---
+++ notify/telegram.go
@@ -0,0 +1,34 @@
+package notify
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"log"
+)
+
+type TelegramNotify struct {
+	Token  string    `yaml:"token"`
+	ChatID string    `yaml:"chatid"`
+}
+
+func (self *TelegramNotify) Notify(message string) {
+	if (len(self.Token) == 0) || (len(self.ChatID) == 0) {
+		log.Println("No credentials")
+		return
+	}
+
+	req := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s", self.Token, self.ChatID, message)
+	resp, err := http.Get(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+}
